b790d4e200a9dbf18fc8667bf72affdfafb232d4
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-03-09 10:59:05 +1000

    Refactor temperature + broken score.

--- Prac_03/broken_score_refactored.py
+++ Prac_03/broken_score_refactored.py
@@ -1,20 +1,29 @@
 
 # CP1404/CP5632 - Practical
 # Broken program to determine score status
+import random
 
 
 def main():
     while True:
-        score = float(input("Enter score: "))
+        score = input("Enter score or 'r' for a random score: ")
+        if score == "r":
+            score = random.randint(1, 100)
+            print("Score = " + str(score))
 
-        if score < 0 or score > 100:
-            print("Invalid score")
-        elif score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("Pass")
-        else:
-            print("Bad")
+        score = float(score)
+        convert_score_to_grade(score)
+
+
+def convert_score_to_grade(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Pass")
+    else:
+        print("Bad")
 
 
 main()
--- Prac_03/temperatures_refactored.py
+++ Prac_03/temperatures_refactored.py
@@ -11,17 +11,27 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
         print(menu)
         choice = input(">>> ").upper()
-    print("Thank you.")
+    print("Exited.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
 
 
 main()
