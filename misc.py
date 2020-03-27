d92d1a0bf163bf793f44686b6a35162fdcd1b743
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-03-27 19:54:19 +1000

    add hex_colours.py

--- misc.py
+++ misc.py
@@ -1,1 +1,10 @@
-print(15 / 3 + 2 * 4)
+
+
+def names_and_ages(names, ages):
+    print(names[ages.index(max(ages))])
+
+
+people = ["Barbra", "Eggman", "Nigle", "Scoot"]
+old = [77, 23, 40, 23]
+
+names_and_ages(people, old)
