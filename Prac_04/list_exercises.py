f29106b05d7536b882268195b8d3abf3c6cbfb9c
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-04-03 17:00:12 +1000

    fix issue with list_exercises.py

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -9,10 +9,13 @@ num_avg = num_sum / 5
 num_dif = max(numbers) - min(numbers)
 num_end = numbers[4]
 
-print("The sum is {}\nThe average is {}\nThe difference between the max and min is {}\nThe last number is {}".format(num_sum, num_avg, num_dif, num_end))
+print("The sum is {}\nThe average is {}\nThe difference between the max and min is {}\nThe last number is {}".
+      format(num_sum, num_avg, num_dif, num_end))
 
 print("*******************************************")
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 user_input = input("Enter Username: ")
 
 if user_input in usernames:
