cd6390f3c0889d139f9920c844e63f805bd8ef16
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-04-04 11:51:35 +1000

    add function use and comments

--- Prac_05/emails.py
+++ Prac_05/emails.py
@@ -3,27 +3,43 @@ CP1404 Practical
 Email / Name Scanner.
 """
 
-email_matrix = {}
-email_input = input("Enter your email: ")
 
-while email_input != '':
-    # Slice the email from the start to the @, then remove any dots, and then capitalise each word.
-    potential_name = email_input[0:email_input.index("@")].replace(".", " ").title()
-
-    user_confirmation = input("Is your name {}? [y|n]: ".format(potential_name)).lower()
-
-    if user_confirmation == "y" or user_confirmation == "":
-        email_matrix[email_input] = potential_name
-    elif user_confirmation == "n":
-        email_matrix[email_input] = input("Name: ")
-
+def main():
+    email_matrix = {}
     email_input = input("Enter your email: ")
 
-print("")
+    while email_input != '':
+        potential_name = extract_name_from_email(email_input)
 
-for email_entry in email_matrix:
-    # Print the name & email. Format: John Wick (john.wick@hitmail.com)
-    print("{} ({})".format(email_matrix[email_entry], email_entry))
+        user_confirmation = input("Is your name {}? [y|n]: ".format(potential_name)).lower()
+        if user_confirmation == "y" or user_confirmation == "":
+            # Potential name was correct, assign to email.
+            email_matrix[email_input] = potential_name
+        elif user_confirmation == "n":
+            # Potential name was incorrect, ask user for name.
+            email_matrix[email_input] = input("Name: ")
+        else:
+            print("Invalid Input.")
+
+        email_input = input("Enter your email: ")
+
+    print("")
+    for email_entry in email_matrix:
+        # Print the name & email. Format: John Wick (john.wick@hitmail.com)
+        print("{} ({})".format(email_matrix[email_entry], email_entry))
+
+
+def extract_name_from_email(email):
+    # 1. Slice it from the start to the @
+    # 2. Then replace any dots with spaces
+    # 3. Then capitalise the start of each word.
+    name = email[0:email.index("@")]\
+        .replace(".", " ")\
+        .title()
+    return name
+
+
+main()
 
 """
     Email: lindsay.ward@jcu.edu.au
