28d69f985a1743ebd3219425033d37b19d33cec0
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-04-06 11:19:34 +1000

    add better naming to main variables

--- Prac_05/word_occurrences.py
+++ Prac_05/word_occurrences.py
@@ -7,7 +7,7 @@ Word occurrence finder.
 text = "One One was a race horse, Two Two was one too, One One won one race, Two Two won one too."
 
 # Dictionary to store each word and the number of mentions:
-word_matrix = {}
+word_count_dictionary = {}
 
 text_edit = text.replace(".", '').replace(",", '')   # Remove any commas and fullstops.
 text_split = text_edit.lower().split(" ")            # Convert to lower case and split at spaces.
@@ -15,12 +15,12 @@ text_split = text_edit.lower().split(" ")            # Convert to lower case and
 max_len = len(max(text_split, key=len))              # Find the length of the longest word.
 
 for word in text_split:
-    if word not in word_matrix:
+    if word not in word_count_dictionary:
         # Add word entry.
-        word_matrix[word] = 1
+        word_count_dictionary[word] = 1
     else:
         # Increment word count.
-        word_matrix[word] += 1
+        word_count_dictionary[word] += 1
 
-for k in sorted(list(word_matrix.keys())):
-    print("{:{}} : {:2}".format(k, max_len, word_matrix[k]))
+for k in sorted(list(word_count_dictionary.keys())):
+    print("{:{}} : {:2}".format(k, max_len, word_count_dictionary[k]))
