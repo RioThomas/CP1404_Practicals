ee7252bc7262cd10ea73f4f3e3ca31fb097f918c
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-04-11 14:39:16 +1000

    add modifications to car.py and used_cars.py

--- Prac_06/car.py
+++ Prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -29,4 +30,8 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def __str__(self):
+        """ Print output in the form: Car, fuel = 42, odometer = 277"""
+        return "{}, fuel = {}, odometer = {}".format(self.name, self.fuel, self.odometer)
 
