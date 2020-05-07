c4bb018063d2d7afb988920b61a1b17a75ff71dc
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-05-07 13:48:15 +1000

    add improvements to unreliable car program + test

--- Prac_08/test_unreliable_car.py
+++ Prac_08/test_unreliable_car.py
@@ -1,15 +1,15 @@
 
 from Prac_08.unreliable_car import UnreliableCar
 
-old_car = UnreliableCar("DeLorean", 160, 50)
+old_car = UnreliableCar("DeLorean", 500, 70)
+bad_car = UnreliableCar("Go-Cart", 20, 30)
 
-print("Make 4 x 40km trips:")
+print("Make 10 x 50km trips with old car:")
+for x in range(10):
+    old_car.drive(50)
+    print(old_car)
 
-old_car.drive(40)
-print(old_car)
-old_car.drive(40)
-print(old_car)
-old_car.drive(40)
-print(old_car)
-old_car.drive(40)
-print(old_car)
+print("\nMake 10 x 50km trips with bad car:")
+for x in range(10):
+    bad_car.drive(1)
+    print(bad_car)
--- Prac_08/unreliable_car.py
+++ Prac_08/unreliable_car.py
@@ -4,30 +4,29 @@ from random import randint
 
 
 class UnreliableCar(Car):
-    failed = False
+    """Create an unreliable car that has a chance to fail every time it is driven.
+    The chance to fail is based on the reliability which is assigned to the object on creation."""
 
     def __init__(self, name, fuel, reliability):
         """Initialise a unreliable car instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
+        self.failed = False
 
     def drive(self, distance):
+        """Generate a random int between 0 and 100 and if it is less than
+        the reliability factor the car will run, if not it will not drive."""
         if randint(0, 100) < self.reliability:
             self.failed = False
-            if distance > self.fuel:
-                distance = self.fuel
-                self.fuel = 0
-            else:
-                self.fuel -= distance
-            self.odometer += distance
-            return distance
         else:
             self.failed = True
+            distance = 0
             return 0
+        super().drive(distance)
 
     def __str__(self):
         """ Print output in the form: Car, fuel = 42, odometer = 277"""
         fail_text = "(Failed)" if self.failed else ""
-        return "{}, fuel = {}, odometer = {} {}".format(self.name, self.fuel, self.odometer, fail_text)
+        return "{} {}".format(super().__str__(), fail_text)
 
 
