8baa70083506c23f4119cf6e219ea9ce56b53f60
RioThomas <53397329+RioThomas@users.noreply.github.com>
2020-05-17 17:28:56 +1000

    add working cleanup_files.py program

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -2,54 +2,32 @@
 CP1404 Practical
 File name cleaning program.
 """
-import shutil
+
 import os
 
 
 def main():
     """Clean file names to ensure correct naming conventions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-
-        # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
-
-
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
-
-
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
         for filename in filenames:
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            new_name = os.path.join(directory_name, clean_filename(filename))
             old_name = os.path.join(directory_name, filename)
+            print("{:50} {:<50}".format(old_name, new_name))
             os.rename(old_name, new_name)
 
 
-# main()
-# demo_walk()
+def clean_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = ""
+    for pos, char in enumerate(filename):
+        if filename[pos].isupper() and filename[pos-1].islower():
+            new_name += " " + filename[pos]
+        else:
+            new_name += filename[pos]
+    new_name = new_name.title().replace(" ", "_").replace("Txt", "txt")[1:]
+    return new_name
+
+
+main()
 
